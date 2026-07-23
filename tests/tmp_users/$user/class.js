d8e2f8d885e53ede69f43ca0f4652e22425dd496
proman1968 <proman62@gmail.com>
2026-07-23 17:03:43 +0300

    удаление SYS

--- tests/tmp_users/$user/class.js
+++
@@ -1,4 +0,0 @@
-export default{
-    label: 'Сидоров С. В.',
-    public: '1342rwehbihwekilvuwelkiteywrikltyi774t3octv7645ty78o4yt9o043y59845tc'
-}
--- tests/tmp_users/635243546352435/$user/class.js
+++
@@ -1,3 +0,0 @@
-export default{
-    label: 'Иванов И.И.',
-}
